36fbcb20fe73cc61274368e5cd298978726c9d2d
Namgel <namgeltamang14@gmail.com>
2026-06-05 09:52:32 +0545

    Use src folder structure

---
+++ src/constants/api.ts
@@ -0,0 +1,9 @@
+const BASE_URL = "/api/v1";
+const VENDOR_URL = `${BASE_URL}/vendor`;
+
+export const VENDOR_API = {
+    // Auth
+    auth: {
+        login: `${VENDOR_URL}/auth/login`,
+    },
+};
