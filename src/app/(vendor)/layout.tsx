c2af3561e83450ed7360751db4ee3ffd2c0e6926
Namgel <namgeltamang14@gmail.com>
2026-06-06 10:03:41 +0545

    Configure tanstack query

--- src/app/(vendor)/layout.tsx
+++ src/app/(vendor)/layout.tsx
@@ -3,8 +3,8 @@
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ReactNode } from "react";
 
-const VendorMainLayout = ({ children }: { children: ReactNode }) => {
+const layout = ({ children }: { children: ReactNode }) => {
     return <AuthProvider>{children}</AuthProvider>;
 };
 
-export default VendorMainLayout;
+export default layout;
--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -3,9 +3,20 @@
 import { store } from "@/redux/store";
 import { ReactNode } from "react";
 import { Provider as ReduxProvider } from "react-redux";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const Provider = ({ children }: { children: ReactNode }) => {
-    return <ReduxProvider store={store}>{children}</ReduxProvider>;
+    return (
+        <ReduxProvider store={store}>
+            <QueryClientProvider client={queryClient}>
+                {children}
+                <ReactQueryDevtools initialIsOpen={false} />
+            </QueryClientProvider>
+        </ReduxProvider>
+    );
 };
 
 export default Provider;
